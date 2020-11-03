539f6bf51be11bae6e92ff75bd4d9f31f5bd99c6
zpskek <wjdghdwns0@gmail.com>
2020-11-03 14:49:50 +0900

    Setting babel for nodejs

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = ()=>{
+  console.log(`✅Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT,handleListening);
