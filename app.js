92fb753fbcab67349378348e13da508045b5c2d6
zpskek <wjdghdwns0@gmail.com>
2020-11-03 15:09:49 +0900

    Setting eslint

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 4000;
 
+const home = app.get("/",(req,res)=>{
+  res.send("hiasd");
+})
+
 const handleListening = ()=>{
   console.log(`✅Listening on: http://localhost:${PORT}`);
 }
