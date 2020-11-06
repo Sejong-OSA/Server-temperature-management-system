871343313800075d7bdf5edd852db71d9ef5f35a
zpskek <wjdghdwns0@gmail.com>
2020-11-06 22:39:40 +0900

    dht11 schema

---
+++ models/dh11.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Dht11Schema = mongoose.Schema({
+  tmp:{
+    type:String,
+    required:true
+  },
+  hum : {
+    type:String,
+    required:true
+  },
+  created_at : {
+    type:Date,
+    default:Date.now
+  }
+});
+
+const model = mongoose.model('Dht11',Dht11Schema);
+
+export default model;
