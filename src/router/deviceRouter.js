a9de0847df5452d72ed7f591cf6512efe4afc7f6
zpskek <wjdghdwns0@gmail.com>
2020-11-20 20:37:01 +0900

    Edit the device

--- src/router/deviceRouter.js
+++ src/router/deviceRouter.js
@@ -3,15 +3,22 @@ import routes from "../routes";
 import { onlyPrivate } from "../middlewares";
 import {
   deviceDetail,
+  getEditDevice,
   getRegister,
+  postEditDevice,
   postRegister,
 } from "../controller/deviceController";
 
 const deviceRouter = express.Router();
 
+// register
 deviceRouter.get(routes.register, onlyPrivate, getRegister);
 deviceRouter.post(routes.register, onlyPrivate, postRegister);
 
 deviceRouter.get(routes.deviceDetail(), deviceDetail);
+
+// edit
+deviceRouter.get(routes.editDevice(), onlyPrivate, getEditDevice);
+deviceRouter.post(routes.editDevice(), onlyPrivate, postEditDevice);
 
 export default deviceRouter;
